86d44db6231536784cd4a4cea94bb2a30f9a1e0e
Ekaluk52003 <ekaluk.p@gmail.com>
2024-08-03 17:01:37 +0700

    db na

--- django_project/settings.py
+++ django_project/settings.py
@@ -99,11 +99,12 @@ TEMPLATES = [
 DATABASES = {
     "default": {
 
-        "ENGINE": os.environ.get("SQL_ENGINE", "django.db.backends.sqlite3"),
-        "NAME": os.environ.get("SQL_DATABASE", BASE_DIR / "db.sqlite3"),
-        "USER": os.environ.get("SQL_USER", "user"),
-        "HOST": os.environ.get("SQL_HOST", "localhost"),
-        "PORT": os.environ.get("SQL_PORT", "5432")    }
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.environ.get('DB_NAME'),
+        'USER': os.environ.get('DB_USER'),
+        'PASSWORD': os.environ.get('DB_PASSWORD'),
+        'HOST': os.environ.get('DB_HOST', 'db'),
+        'PORT': os.environ.get('DB_PORT', '5432'),   }
 }
 
 # Password validation
