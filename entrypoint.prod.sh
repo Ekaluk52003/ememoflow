2e4df786c3ce565b7cd5d01a0e8d59ef8748177e
Ekaluk52003 <ekaluk.p@gmail.com>
2024-08-13 22:21:51 +0700

    update entrypoint

--- entrypoint.prod.sh
+++ entrypoint.prod.sh
@@ -11,4 +11,14 @@ then
     echo "PostgreSQL started"
 fi
 
+# Collect static files
+echo "Collecting static files..."
+python manage.py collectstatic --noinput
+
+# Apply database migrations
+echo "Applying database migrations..."
+python manage.py migrate
+
+# Start Gunicorn
+echo "Starting Gunicorn..."
 exec "$@"
